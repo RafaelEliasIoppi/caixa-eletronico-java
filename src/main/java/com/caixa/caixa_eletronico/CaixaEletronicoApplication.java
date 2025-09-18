30ac7707452ae208bfc55c63198cb8f98ebf049a
Rafael Elias Ioppi <141973836+RafaelEliasIoppi@users.noreply.github.com>
2025-09-18 13:24:23 +0000

    rafael

--- src/main/java/com/caixa/caixa_eletronico/CaixaEletronicoApplication.java
+++ src/main/java/com/caixa/caixa_eletronico/CaixaEletronicoApplication.java
@@ -8,14 +8,15 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
 	servers = {
-		@Server(url = "https://urban-acorn-g4qrqppjw7p43jq9-3335.app.github.dev/")
-	}
-)
-@SpringBootApplication
-public class CaixaEletronicoApplication {
+			@Server(url = "https://urban-acorn-g4qrqppjw7p43jq9-3335.app.github.dev/")
+				}
+				)
+				@SpringBootApplication
+				public class CaixaEletronicoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CaixaEletronicoApplication.class, args);
-	}
+					public static void main(String[] args) {
+							SpringApplication.run(CaixaEletronicoApplication.class, args);
+								}
 
-}
+								}
+								
